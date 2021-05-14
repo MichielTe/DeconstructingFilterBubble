fce475c1db29437ea4a5ff322110c6f7f5dcc4e1
MichielTe <michiel@teblick.com>
2021-05-14 18:06:01 +0200

    added diversity plot

--- data_plotting.py
+++ data_plotting.py
@@ -6,15 +6,18 @@ from main import distance
 
 def plot_local_consumption(results, N):
     cases = ["recommendation", "no_recommendation", "oracle"]
+    nb_items_chosen = 20
     for case in cases:
-        nb_items_chosen = len(results[case][0][0])
         consumption = []
+        distances = []
         for t in range(1, nb_items_chosen):
-            distances = []
-            for population in results[case]:
-                for user in population:
-                    distances.append(distance(N, user[t-1], user[t]))
+            for params in results:
+                result = results[params]
+                for population in result[case]:
+                    for user in population:
+                        distances.append(distance(N, user[t-1], user[t]))
             consumption.append(np.mean(distances))
+            print("case: ", case, "\t t:", t)
         plt.plot(range(1, nb_items_chosen), consumption, label=case)
     plt.xlabel("t")
     plt.ylabel("average distance")
@@ -22,10 +25,52 @@ def plot_local_consumption(results, N):
     plt.show()
 
 
+def diversity(items):
+    distances = []
+    N = 200
+    for i, item in enumerate(items):
+        for j in range(i, len(items)):
+            distances.append(distance(N, item, items[j]))
+    result = (1/(N*20*19))*sum(distances)
+    return result
+
+
+def plot_item_diversity(results, N):
+    cases = ["recommendation", "no_recommendation", "oracle"]
+    temp = dict()
+
+    for case in cases:
+        print("starting case: ", case)
+        for i in rhos:
+            temp[i] = []
+        for params in results:
+            rho, beta, gamma, sigma = params
+            result = results[params]
+            print(params)
+            for population in result[case]:
+                for user in population:
+                    temp[rho].append(diversity(user))
+        y_values = []
+        for i in rhos:
+            y_values.append(np.mean(temp[i]))
+        plt.plot(rhos, y_values, label=case)
+
+    plt.xlabel("rho")
+    plt.ylabel("item diversity")
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
+    rhos = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
+    betas = [0, 0.4, 0.8, 1, 2, 5]
+    gammas = [0, 0.3, 0.6, 1, 5]  # this is the same as alpha in the original code
+    sigmas = [0.25, 0.5, 1, 2, 4]
+
     with open("results.pickle", "rb") as file:
         results = pickle.load(file)
 
     N = 200
-    plot_local_consumption(results, 200)
+    #plot_local_consumption(results, N)
+    plot_item_diversity(results, N)
 
